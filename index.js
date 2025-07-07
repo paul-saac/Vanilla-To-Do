a1a6852d2206f11c98d3587b5a3759b07329c547
paul-saac <johnpaul.saac.legaspi@gmail.com>
2025-07-07 19:42:45 +0800

    restart

--- index.js
+++ index.js
@@ -25,29 +25,3 @@ function addTodo() {
         todoInput.value = "";
     }
 }
-
-
-function addTodo() {
-    const todoText = todoInput.value.trim();
-    if(todoText.length > 0) {
-        allTodos.push(todoText);
-        createTodoItem(todoText);
-        todoInput.value = "";
-    }
-}
-function addTodo() {
-    const todoText = todoInput.value.trim();
-    if(todoText.length > 0) {
-        allTodos.push(todoText);
-        createTodoItem(todoText);
-        todoInput.value = "";
-    }
-}
-function addTodo() {
-    const todoText = todoInput.value.trim();
-    if(todoText.length > 0) {
-        allTodos.push(todoText);
-        createTodoItem(todoText);
-        todoInput.value = "";
-    }
-}
