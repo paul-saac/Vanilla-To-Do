38c379e11201bd61d079b4558a71258605ce5bc2
paul-saac <johnpaul.saac.legaspi@gmail.com>
2025-07-29 16:27:10 +0800

    yes

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoListUL = document.getElementById('todo-list');
 
-let allTodos = [];
-
-// Load todos from Firestore when page loads
-window.addEventListener('DOMContentLoaded', loadTodos);
-
 todoForm.addEventListener('submit', function(e){
     e.preventDefault();
     addTodo();
