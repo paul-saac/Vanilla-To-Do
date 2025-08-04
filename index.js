aea2c1f5deca1fb16f37de90c55c8c80201f89f5
paul-saac <johnpaul.saac.legaspi@gmail.com>
2025-08-04 19:15:45 +0800

    cleaning files and code

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore-compat.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+  
+
+// Firebase configuration using compat libraries
+const firebaseConfig = {
+    apiKey: "AIzaSyAuUnlvwPdm_npCVjS3rXJfFZHMwIZP0ZM",
+    authDomain: "vanilla-to-do.firebaseapp.com",
+    projectId: "vanilla-to-do",
+    storageBucket: "vanilla-to-do.firebasestorage.app",
+    messagingSenderId: "783719500831",
+    appId: "1:783719500831:web:b24b6697a39af090e4592a",
+    measurementId: "G-9KB44NXMFF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = firebase.firestore();
+
+
+
+
+
 const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoListUL = document.getElementById('todo-list');
